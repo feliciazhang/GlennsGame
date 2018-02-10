3cf57ad0c60fc8b0ad7e1b9602a6fc51e9017995
Felicia Zhang <fy.zhang2016@gmail.com>
2018-02-10 18:17:54 -0500

    bd and glenn moving

--- Assets/Scripts/BarryDwightCtrl.cs
+++ Assets/Scripts/BarryDwightCtrl.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarryDwightCtrl : MonoBehaviour {
+    Rigidbody2D barryRB;
+    SpriteRenderer barryRenderer;
+
+    private bool movingUp;
+    private float speed;
+    private Transform destPoint;
+    public Transform[] points;
+    public int pointSelection;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        barryRB = GetComponent<Rigidbody2D>();
+        barryRenderer = GetComponent<SpriteRenderer>();
+        destPoint = points[pointSelection];
+        speed = Random.Range(4.0f, 10.0f);
+    }
 	
-	// Update is called once per frame
+	// Barry Dwight moves up and down at random speed
 	void Update () {
-		
-	}
+        barryRB.transform.position = Vector3.MoveTowards(barryRB.transform.position,
+            destPoint.position, Time.deltaTime * speed);
+        if (barryRB.transform.position == destPoint.position) {
+            pointSelection = pointSelection == points.Length - 1 ? 0 : pointSelection + 1;
+            destPoint = points[pointSelection];
+            speed = Random.Range(4.0f, 10.0f);
+        }
+    }
 }
--- Assets/Scripts/GlennCtrl.cs
+++ Assets/Scripts/GlennCtrl.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class GlennCtrl : MonoBehaviour {
     Rigidbody2D glennRB;
     SpriteRenderer glennRenderer;
+    Animator glennAnim;
 
-    bool facingRight;
-    
-	// Use this for initialization
-	void Start () {
+    public float maxSpeed;
+    private bool facingRight;
+
+    // Use this for initialization
+    void Start () {
         glennRB = GetComponent<Rigidbody2D>();
         glennRenderer = GetComponent<SpriteRenderer>();
+        glennAnim = GetComponent<Animator>();
+        facingRight = true;
 	}
 	
-	// Update is called once per frame
+	// Glenn moves right and left, can shoot weapon
 	void Update () {
         float move = Input.GetAxis("Horizontal");
-        if(move < 0 == facingRight) {
+
+        glennAnim.SetFloat("MoveSpeed", Mathf.Abs(move));
+        if (move < 0 == facingRight) {
             Turn();
         }
+        glennRB.velocity = new Vector2(move * maxSpeed, glennRB.velocity.y);
 	}
 
     //Switch directions
     void Turn () {
-
+        facingRight = !facingRight;
+        glennRenderer.flipX = !glennRenderer.flipX;
     }
 }
