4ca870b5661ea03b0b89088c7fa6bba511828da5
Felicia Zhang <fy.zhang2016@gmail.com>
2018-02-10 10:52:23 -0500

    Init

---
+++ Assets/Scripts/GlennCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlennCtrl : MonoBehaviour {
+    Rigidbody2D glennRB;
+    SpriteRenderer glennRenderer;
+
+    bool facingRight;
+    
+	// Use this for initialization
+	void Start () {
+        glennRB = GetComponent<Rigidbody2D>();
+        glennRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        if(move < 0 == facingRight) {
+            Turn();
+        }
+	}
+
+    //Switch directions
+    void Turn () {
+
+    }
+}
